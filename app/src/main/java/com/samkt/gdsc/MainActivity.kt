62512427d948ec42f95fa880fb506c5b26fbefdd
sam muigai <muigai.samuel21@students.dkut.ac.ke>
2023-09-27 12:21:55 +0300

    Initial commit

---
+++ app/src/main/java/com/samkt/gdsc/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.samkt.gdsc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.samkt.gdsc.ui.theme.GDSCTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GDSCTheme {
+            }
+        }
+    }
+}
